9adda4cfee2cdf34deafcb80a2582de0ea0eee22
Jana Stefanie <s2400789@BC-CWRW4DVTW1-Kannettava-tietokone.local>
2025-01-24 12:17:00 +0200

    fix bug in ThemeProvider

--- src/context/ThemeProvider.tsx
+++ src/context/ThemeProvider.tsx
@@ -1,6 +1,8 @@
 import { ThemeContext } from "@emotion/react";
 import { PaletteMode } from "@mui/material";
 import { ReactNode, useMemo, useState } from "react";
+import {ThemeProvider as MUIThemeProvider} from "@mui/material/styles"
+import { createAppTheme } from "../theme/theme";
 
 // we need interface to show what props its gonna Be
 
@@ -8,7 +10,7 @@ interface Props {
   children: ReactNode; // is a component: a node point of access to other things - hover over to see
 }
 
-export const ThemeProvider = ([children]): Props => {
+export const ThemeProvider = ({children}: Props) => { // announcing the children are of type props - ch are props (interface props are chi of node) - out of bracket: return type is props (was wrong)
   const [mode, setMode] = useState<PaletteMode>("light");
 
   const themeContext = useMemo(
